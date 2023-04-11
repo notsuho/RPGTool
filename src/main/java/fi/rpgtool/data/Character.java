959eb38b45b797c361b4dc961b84d46135629871
TotalledZebra <Holappa57@gmail.com>
2023-04-11 16:14:27 +0300

    Test commit

---
+++ src/main/java/fi/rpgtool/data/Character.java
@@ -0,0 +1,11 @@
+package fi.rpgtool.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Character {
+
+    private final List<Pair<String, Integer>> statistics = new ArrayList<>();
+    private final List<Pair<String, Integer>> abilities = new ArrayList<>();
+
+}
---
+++ src/main/java/fi/rpgtool/data/Pair.java
@@ -0,0 +1,28 @@
+package fi.rpgtool.data;
+
+public class Pair<L, R> {
+
+    L left;
+    R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    public void setLeft(L left) {
+        this.left = left;
+    }
+
+    public void setRight(R right) {
+        this.right = right;
+    }
+}
